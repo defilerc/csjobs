d1b3e13c29615de309c5ef48659d24ed948ea0b7
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-07-09 20:34:01 +0300

    dotnet new console

---
+++ ZigZag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZigZag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
