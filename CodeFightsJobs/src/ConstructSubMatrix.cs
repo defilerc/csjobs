3924eab494df657a086ac4f3158a4b86e1955751
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-07-10 22:38:52 +0300

    solved construct submatrix

---
+++ CodeFightsJobs/src/ConstructSubMatrix.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeFights
+{
+    public class ConstructSubMatrix
+    {
+        public int[][] constructSubmatrix(int[][] matrix, int[] rowsToDelete, int[] columnsToDelete) 
+        {
+            var submatrix = new List<List<int>>();
+
+            var iDest = 0;
+            var jDest = 0;
+
+            for (var i = 0; i < matrix.Length; i++) {
+                if (rowsToDelete.Contains(i)) {
+                    continue;
+                }
+                var row = new List<int>();
+
+                for (var j = 0; j < matrix[0].Length; j++) {
+                    if (columnsToDelete.Contains(j)) {
+                        continue;
+                    }
+                    jDest++;
+
+                    row.Add(matrix[i][j]);
+                }
+
+                iDest++;
+                jDest = 0;
+
+                submatrix.Add(row);
+            }
+
+            return submatrix.Select(a => a.ToArray()).ToArray();
+        }
+    }
+}
